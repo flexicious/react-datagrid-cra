b8e8b133d14a78611a656649a1681d6f5a837db7
flexicious <flexicious@gmail.com>
2023-03-09 21:50:58 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import "@euxdt/grid-core/icons.css";
+import "@euxdt/grid-core/styles.css";
+import './App.css';
+import Dashboard from './dashboard/dashboard';
+
+
+
+function App() {
+  return (<Dashboard/>
+  );
+}
+
+export default App;
